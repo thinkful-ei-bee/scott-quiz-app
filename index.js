0336d1ace839ccdd14bcb1af8a6e16eaa6b19681
Scott Dowding <scottddowding1279@gmail.com>
2019-02-27 16:31:03 -0500

    completed quiz app with store

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 /* global $ */
 
+const store = {
+  page: '',
+  currentQuestion: 0,
+  userAnswers: [],
+  correctAnswers: 0,
+  correctAnswer: '',
+};
+
 const questionSet = [
   { 
     number: 1,
@@ -9,6 +17,7 @@ const questionSet = [
     ans2: '"Smokey" Joe Wood', 
     ans3: 'Cy Young', 
     ans4: 'Roger Clemens',
+    correctAns: 2,
   }, 
 
   {
@@ -18,6 +27,7 @@ const questionSet = [
     ans2: 'It sits on land belonging to the Fenway family estate.', 
     ans3: 'It was named for a former Boston mayor.', 
     ans4: 'It is named for a district in the city.',
+    correctAns: 3,
   }, 
 
   {
@@ -27,6 +37,7 @@ const questionSet = [
     ans2: 'David Halberstam', 
     ans3: 'John Updike', 
     ans4: 'Bill Bryson',
+    correctAns: 1,
   }, 
   {
     number: 4, 
@@ -35,6 +46,7 @@ const questionSet = [
     ans2: 'Frank Malzone', 
     ans3: 'Bobby Doerr', 
     ans4: 'Joe Cronin',
+    correctAns: 0
   }, 
   {
     number: 5,
@@ -43,6 +55,7 @@ const questionSet = [
     ans2: '"God Bless America" by Kate Smith', 
     ans3: '"Talkin\' Baseball" by Terry Cashman', 
     ans4: '"Sweet Caroline" by Neil Diamond',
+    correctAns: 3,
   }, 
   {
     number: 6,
@@ -51,6 +64,7 @@ const questionSet = [
     ans2: 'Chicken', 
     ans3: 'Tacos', 
     ans4: 'Liver and onions',
+    correctAns: 1,
   }, 
   {
     number: 7,
@@ -59,6 +73,7 @@ const questionSet = [
     ans2: 'He tossed the first no-hitter in Red Sox history.', 
     ans3: 'He struck out seven Yankees in a row.', 
     ans4: 'He was the last major-league pitcher to win 30 games.',
+    correctAns: 0,
   }, 
   {
     number: 8,
@@ -67,6 +82,7 @@ const questionSet = [
     ans2: '"The Impossible Dream"', 
     ans3: '"The Revenge of the Red Sox"', 
     ans4: '"Comeback of the Century"',
+    correctAns: 1,
   }, 
   {
     number: 9,
@@ -75,6 +91,7 @@ const questionSet = [
     ans2: 'Bill Lee', 
     ans3: 'Babe Ruth', 
     ans4: 'Mel Parnell',
+    correctAns: 3,
   }, 
   {
     number: 10,
@@ -83,76 +100,53 @@ const questionSet = [
     ans2: 'Leading the majors with 29 home runs in 1919', 
     ans3: 'Pitching 29.1 consecutive scoreless innings in the World Series', 
     ans4: 'Stealing seven bases in 1919',
+    correctAns: 2,
   }
 ];
 
-const ANSWERS = [ 
-  'Cy Young', 
-  'It is named for a district in the city.', 
-  'David Halberstam', 
-  'Jimmie Foxx', 
-  '"Sweet Caroline" by Neil Diamond', 
-  'Chicken', 
-  'He was the winning pitcher when the Red Sox won the 1967 pennant.', 
-  '"The Impossible Dream"', 
-  'Mel Parnell', 
-  'Pitching 29.1 consecutive scoreless innings in the World Series'
-];
-
-let questionNum = 1;
-
-let correctAnswers = 0;
-
-const correctFeedback = `
-  <main class="feedback-page" role="main">
-    <h2>Correct!</h2>
-    <img src="https://media.giphy.com/media/BJJH3HK2cPWpy/giphy.gif" alt="Homerun">
-    <button id="js-next-button">Next</button>
-  </main>
-`;
-
-function questionTemplate(correctAnswers, question, questionsAnswered) {
+function questionTemplate() {
   return `
     <main id="question-page" role="main">
-    <h2 id="question">${question.text}</h2>
+      <h2 id="question">${questionSet[store.currentQuestion - 1].text}</h2>
     
-    <form>
-      <fieldset>
-        <label>
-          <input class="answer" type="radio" name="option" checked></input>
-          <span>${question.ans1}</span>
-        </label>
+      <form>
+        <fieldset>
+          <label>
+            <input class="answer" type="radio" name="option" value="0" checked></input>
+           <span>${questionSet[store.currentQuestion - 1].ans1}</span>
+          </label>
   
-        <label>
-          <input class="answer" type="radio" name="option"></input>
-          <span>${question.ans2}</span>
-        </label>
+          <label>
+            <input class="answer" type="radio" name="option" value="1"></input>
+            <span>${questionSet[store.currentQuestion - 1].ans2}</span>
+          </label>
   
-        <label>
-          <input class="answer" type="radio" name="option"></input>
-          <span>${question.ans3}</span>
-        </label>
+          <label>
+            <input class="answer" type="radio" name="option" value="2"></input>
+            <span>${questionSet[store.currentQuestion - 1].ans3}</span>
+          </label>
   
-        <label>
-          <input class="answer" type="radio" name="option"></input>
-          <span>${question.ans4}</span>
-        </label>
-      </fieldset>  
-      <button id="js-submit-button">Submit</button>
+          <label>
+            <input class="answer" type="radio" name="option" value="3"></input>
+           <span>${questionSet[store.currentQuestion - 1].ans4}</span>
+         </label>
+        </fieldset>  
+        <button id="js-submit-button">Submit</button>
+      </form>
 
-    </form>
-
-    <div id="status-bar">
-      <span id="question-count">Question: ${question.number}/10</span>
-      <span id="score-count">Score: ${correctAnswers}/${questionsAnswered}</span>
-    </div>
-  </main>
+      <div id="status-bar">
+        <span id="question-count">Question: ${store.currentQuestion}/10</span>
+        <span id="score-count">Score: ${store.correctAnswers}/${store.currentQuestion}</span>
+     </div>
+    </main>
   `;
 }
 
 function handleStartButton() {
-  $('#js-start-button').click(function() {
-    nextQuestion();
+  $('#container').on('click', '#js-start-button', function() {
+    store.currentQuestion = 1;
+    store.page = 'questions';
+    render();
   });
 }
 
@@ -160,91 +154,110 @@ function handleSubmitButton() {
   $('#container').on('click', '#js-submit-button', function(event) {
     event.preventDefault();
 
-    const answer = $('input:checked').siblings('span');
+    const answer = Number($('input[name=\'option\']:checked').val());
+    store.userAnswers.push(answer);
 
-    const userIsCorrect = checkUserAnswer(answer);
-    if(userIsCorrect) {
-      generateCorrectFeedback();
+    if (store.userAnswers[store.userAnswers.length - 1] === questionSet[store.currentQuestion - 1].correctAns) {
+      store.page = 'correctAnswers';
+      store.correctAnswers++;
     } else {
-      generateIncorrectFeedback();
+      store.page = 'incorrectAnswers';
     }
+
+    store.questionNum++;
+    render();
   });
 }
 
 function handleNextButton() {
   $('#container').on('click', '#js-next-button', function() {
 
-    if(questionNum === 10) {
-      createResultsPage(correctAnswers);
+    if(store.currentQuestion === 10) {
+      store.page = 'results';
     } else {
-      incrementQuestion();
-      nextQuestion();
+      store.currentQuestion++;
+      store.page = 'questions';
     }
+
+    render();
   });
 }
 
 function handleRestartButton() {
   $('#container').on('click', '#js-restart-button', function() {
+    store.currentQuestion = 1;
+    store.correctAnswers = 0;
+    store.page = 'intro';
 
-    questionNum = 1;
-
-    correctAnswers = 0;
-
-    nextQuestion();
+    render();
   });
 }
 
-function nextQuestion() {
+function render() {
 
-  const question = questionSet[questionNum - 1];
+  if (store.page === 'questions' || store.page === 'intro') {
+    $('#container').html(questionTemplate());
+    return;
+  } 
 
-  const questionsAnswered = questionNum - 1;
+  if (store.page === 'correctAnswers') {
+    $('#container').html(generateCorrectFeedback());
+    return;
+  }
 
-  $('#container').html(questionTemplate(correctAnswers, question, questionsAnswered));
-}
+  if (store.page === 'incorrectAnswers') {
+    if (questionSet[store.currentQuestion - 1].correctAns === 0) {
+      store.correctAnswer = questionSet[store.currentQuestion - 1].ans1;
+    }
 
-function checkUserAnswer(answer) {
-  if(answer.text() === ANSWERS[questionNum - 1]) {
-    return true;
-  } else {
-    return false;
+    if (questionSet[store.currentQuestion - 1].correctAns === 1) {
+      store.correctAnswer = questionSet[store.currentQuestion - 1].ans2;
+    }
+
+    if (questionSet[store.currentQuestion - 1].correctAns === 2) {
+      store.correctAnswer = questionSet[store.currentQuestion - 1].ans3;
+    }
+
+    if (questionSet[store.currentQuestion - 1].correctAns === 3) {
+      store.correctAnswer = questionSet[store.currentQuestion - 1].ans4;
+    }
+
+    $('#container').html(generateIncorrectFeedback());
+    return;
+  }
+
+  if (store.page === 'results') {
+    $('#container').html(createResultsPage());
   }
 }
 
 function generateCorrectFeedback() {
-  $('#container').html(correctFeedback);
-  incrementCorrectAnswers();
-}
-
-function generateIncorrectFeedback() {
-  $('#container').html(incorrectFeedback(questionNum));
-}
-
-function incorrectFeedback(questionNum) {
   return `
-    <main class="feedback-page" role="main">
-      <h2>Nope! It was ${ANSWERS[questionNum - 1]}!</h2>
-      <img src="https://media.giphy.com/media/iEQWZ7LxibbwI/giphy.gif" alt="Strikeout">
-      <button id="js-next-button">Next</button>
-    </main>
+  <main class="feedback-page" role="main">
+    <h2>Correct!</h2>
+    <img src="https://media.giphy.com/media/BJJH3HK2cPWpy/giphy.gif" alt="Homerun">
+    <button id="js-next-button">Next</button>
+  </main>
 `;
 }
 
-function incrementQuestion() {
-  questionNum++;
-}
-
-function incrementCorrectAnswers() {
-  correctAnswers++;
+function generateIncorrectFeedback() {
+  return `
+  <main class="feedback-page" role="main">
+    <h2>Nope! It was ${store.correctAnswer}!</h2>
+    <img src="https://media.giphy.com/media/iEQWZ7LxibbwI/giphy.gif" alt="Strikeout">
+    <button id="js-next-button">Next</button>
+  </main>
+`;
 }
 
 function createResultsPage(correctAnswers) {
-  $('#container').html(`
+  return `
     <main id="final-page" role="main">
-      <h2>Final Score: ${correctAnswers} out of 10</h2>
+      <h2>Final Score: ${store.correctAnswers} out of 10</h2>
       <button id="js-restart-button">Play Again?</button>
     </main>
-  `);
+  `;
 }
 
 function handleButtons() {
